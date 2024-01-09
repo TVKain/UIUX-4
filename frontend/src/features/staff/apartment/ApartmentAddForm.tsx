4698cdeffef6160d47a2b9d4949ac9057a1a9d75
tvkain <tvkain.it@gmail.com>
2024-01-09 21:04:10 +0700

    .

--- frontend/src/features/staff/apartment/ApartmentAddForm.tsx
+++ frontend/src/features/staff/apartment/ApartmentAddForm.tsx
@@ -24,7 +24,10 @@ const ApartmentAddForm = () => {
     },
     validationSchema: yup.object<CreateApartmentRequest>({
       name: yup.string().required('Tên căn hộ không được để trống'),
-      size: yup.number().required('Kích cỡ căn hộ không được để trống'),
+      size: yup
+        .number()
+        .required('Kích cỡ căn hộ không được để trống')
+        .min(0, 'Kích cỡ căn hộ phải lớn hơn 0'),
       BuildingId: yup.string().required('Chung cư không được để trống'),
     }),
 
