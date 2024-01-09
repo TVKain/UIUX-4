f74bb575c4d1929762763190bc62051573f1dd96
tvkain <tvkain.it@gmail.com>
2024-01-09 20:51:54 +0700

    .

--- frontend/src/api/user-info/updateUserInfo.ts
+++ frontend/src/api/user-info/updateUserInfo.ts
@@ -4,7 +4,7 @@ import { url } from '../../config/constants';
 
 import axios from 'axios';
 
-interface UpdateUserInfoRequest extends Omit<UserInfo, 'UserId'> {}
+interface UpdateUserInfoRequest extends Omit<Omit<Omit<UserInfo, 'UserId'>, 'joinDate'>, 'leaveDate'> {}
 
 async function updateUserInfo(data: UpdateUserInfoRequest) {
   const result = await axios.put(`${url}/user-infos`, data);
